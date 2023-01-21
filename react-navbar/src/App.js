5244d3437c66c665e4e172af905bd129dc27b819
Anish <anish.gyawali7@gmail.com>
2023-01-20 22:17:26 -0600

    added comment to code and removed unused code

--- react-navbar/src/App.js
+++ react-navbar/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
+    /* The Router component is a higher-order component that provides the routing functionality to the application */
     <Router>
       <Navbar />
     </Router>
